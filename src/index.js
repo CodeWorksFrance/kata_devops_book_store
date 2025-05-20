9404f9816ebda80cca9e5f9553607818f0dda0fc
Dimitri Merejkowsky <dimitri.merejkowsky@external.engie.com>
2025-05-20 15:53:02 +0200

    Add more HTML on the home page

--- src/index.js
+++ src/index.js
@@ -27,8 +27,27 @@ function normalizePort(val) {
 }
 
 
+const homeHtml = `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Book Store Web App</title>
+</head>
+<body>
+    <h1>Welcome to the Book Store</h1>
+    <p>Use the following endpoints:</p>
+    <ul>
+        <li><a href="/books">/books</a>: List all the books in the catalog</li>
+        <li><a href="/crash">Don't click here!</a></li>
+    </ul>
+</body>
+</html>
+`;
+
+
 app.get("/", (req, res) => {
-    res.send("Hello World!");
+    res.send(homeHtml);
 });
 
 app.get("/books", (req, res) => {
