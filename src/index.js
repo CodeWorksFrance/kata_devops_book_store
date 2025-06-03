82037c982c8f490d03f4c9089df176d094514f6a
Dimitri Merejkowsky <dimitri.merejkowsky@external.engie.com>
2025-06-03 16:23:25 +0200

    Add morgan logger

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const { getBooks } = require("@codeworksfrance/catalog")
+var logger = require('morgan');
 
 const app = express();
+app.use(logger('dev'));
+
+
 const port = normalizePort(process.env.PORT)
 
 
@@ -44,6 +48,18 @@ const homeHtml = `
 </body>
 </html>
 `;
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
 
 
 app.get("/", (req, res) => {
