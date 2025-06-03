20603a3d240503e94ef577baf04509883a479fba
Dimitri Merejkowsky <dimitri.merejkowsky@external.engie.com>
2025-06-03 16:38:14 +0200

    Autocollect onsole.log

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 const APP_INSIGHTS_CONNECTION_STRING = "InstrumentationKey=5a5a7d93-47f6-4655-93be-e3870f5f6d6f;IngestionEndpoint=https://francecentral-1.in.applicationinsights.azure.com/;LiveEndpoint=https://francecentral.livediagnostics.monitor.azure.com/;ApplicationId=87ccec3a-775c-4469-8dc0-c0fbb3afcfef"
 let appInsights = require('applicationinsights');
 appInsights.setup(APP_INSIGHTS_CONNECTION_STRING)
-    .start();
+  .setAutoCollectConsole(true, true)
+  .start();
 const express = require("express");
 const { getBooks } = require("@codeworksfrance/catalog")
 var logger = require('morgan');
@@ -21,7 +22,7 @@ const port = normalizePort(process.env.PORT)
  */
 
 function normalizePort(val) {
-  if(val === undefined) {
+  if (val === undefined) {
     return 3000
   }
   var port = parseInt(val, 10);
@@ -57,7 +58,7 @@ const homeHtml = `
 `;
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -70,12 +71,12 @@ app.use(function(err, req, res, next) {
 
 
 app.get("/", (req, res) => {
-    res.send(homeHtml);
+  res.send(homeHtml);
 });
 
 app.get("/books", (req, res) => {
-    const books = getBooks()
-    res.send(books)
+  const books = getBooks()
+  res.send(books)
 });
 
 app.get("/crash", (req, res) => {
@@ -86,5 +87,5 @@ app.get("/crash", (req, res) => {
 });
 
 app.listen(port, () => {
-    console.log(`App is listening at ${port}!`);
+  console.log(`App is listening at ${port}!`);
 });
