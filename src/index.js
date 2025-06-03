4c6165675f671e799e9b8334197e52626b0f55ce
Dimitri Merejkowsky <dimitri.merejkowsky@external.engie.com>
2025-06-03 16:28:23 +0200

    Setup App Insights
    Note: hard-code the connection string for now

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
+const APP_INSIGHTS_CONNECTION_STRING = "InstrumentationKey=5a5a7d93-47f6-4655-93be-e3870f5f6d6f;IngestionEndpoint=https://francecentral-1.in.applicationinsights.azure.com/;LiveEndpoint=https://francecentral.livediagnostics.monitor.azure.com/;ApplicationId=87ccec3a-775c-4469-8dc0-c0fbb3afcfef"
+let appInsights = require('applicationinsights');
+appInsights.setup(APP_INSIGHTS_CONNECTION_STRING)
+    .start();
 const express = require("express");
 const { getBooks } = require("@codeworksfrance/catalog")
 var logger = require('morgan');
 
+
 const app = express();
 app.use(logger('dev'));
+
+
 
 
 const port = normalizePort(process.env.PORT)
