327978b7d8d5e64825c8d42657d7ec0cca5182a0
Dimitri Merejkowsky <dimitri.merejkowsky@external.engie.com>
2025-06-12 14:40:01 +0200

    Also log incoming requests and outgoing responses
    Note: no 'request id', by design

--- src/app.js
+++ src/app.js
@@ -4,6 +4,32 @@ const app = express()
 
 const { getBooks } = require('@codeworksfrance/catalog')
 const indexHtml = require('./index.js')
+const { logger } = require('./logs.js')
+
+// Note : must be first
+app.use((req, res, next) => {
+  const { method, originalUrl } = req
+  logger.info(
+    {
+      method,
+      originalUrl
+    },
+    'Got request'
+  )
+
+  res.on('finish', () => {
+    const { statusCode } = res
+    logger.info(
+      {
+        method,
+        originalUrl,
+        statusCode
+      },
+      'Response sent' )
+  })
+
+  next()
+})
 
 app.get('/', (req, res) => {
   res.send(indexHtml)
@@ -19,6 +45,12 @@ app.get('/crash', (req, res) => {
   const books = getBooks()
   const book = books[badId]
   res.send(book.title)
+})
+
+// Note : must be last
+app.use((err, req, res, next) => {
+  logger.error(err)
+  res.status(500).send('Internal Server Error')
 })
 
 module.exports = app
--- src/main.js
+++ src/main.js
@@ -8,11 +8,6 @@ const { logger } = require('./logs.js')
 
 const port = getListeningPort()
 
-app.use((err, req, res, next) => {
-  logger.error(err)
-  res.status(500).send('Internal Server Error')
-})
-
 logger.info('Starting the application')
 app.listen(port, () => {
   logger.info(`Lstening on ${port}`)
