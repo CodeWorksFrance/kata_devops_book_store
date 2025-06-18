2eab4971d69fab87d028ce091dca84bcae1bfafa
vincent-codeworks <vincent.bourgeois@codeworks.fr>
2025-06-18 09:53:02 +0200

    task: add logs

--- src/main.js
+++ src/main.js
@@ -1,3 +1,7 @@
+const appInsights = require('applicationinsights');
+appInsights.setup("InstrumentationKey=3f755261-82c7-4c44-a2af-d80bc59f9c40;IngestionEndpoint=https://francecentral-1.in.applicationinsights.azure.com/;LiveEndpoint=https://francecentral.livediagnostics.monitor.azure.com/;ApplicationId=efa02eb3-9a84-428e-b3a6-8e781d69a913")
+appInsights.start();
+
 const app = require('./app.js')
 const { getListeningPort } = require('./env.js')
 
